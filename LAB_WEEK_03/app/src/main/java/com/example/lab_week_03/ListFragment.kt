5cbaccef53c2d76dec3788995cb53c6f1fd0e512
seronnnn <163845781+seronnnn@users.noreply.github.com>
2025-09-09 11:35:20 +0700

    Commit No. 2 – add coffee list & detail fragment

--- LAB_WEEK_03/app/src/main/java/com/example/lab_week_03/ListFragment.kt
+++ LAB_WEEK_03/app/src/main/java/com/example/lab_week_03/ListFragment.kt
@@ -8,78 +8,71 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class MainFragment : Fragment() {
+class ListFragment : Fragment(), View.OnClickListener {
 
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var coffeeListener: CoffeeListener
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
+        if (context is CoffeeListener) {
+            coffeeListener = context
+        } else {
+            throw RuntimeException("Must implement CoffeeListener")
+        }
         Log.d(TAG, "onAttach")
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d(TAG, "onCreate")
-
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+        Log.d(TAG, "onCreate")
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         Log.d(TAG, "onCreateView")
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        return inflater.inflate(R.layout.fragment_list, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val coffeeList = listOf<View>(
+            view.findViewById(R.id.affogato),
+            view.findViewById(R.id.americano),
+            view.findViewById(R.id.latte)
+        )
+
+        coffeeList.forEach {
+            it.setOnClickListener(this)
+        }
+
         Log.d(TAG, "onViewCreated")
     }
 
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG, "onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG, "onStop")
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        Log.d(TAG, "onDestroyView")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "onDestroy")
-    }
-
-    override fun onDetach() {
-        super.onDetach()
-        Log.d(TAG, "onDetach")
+    override fun onClick(v: View?) {
+        v?.let { coffee ->
+            Log.d(TAG, "Item clicked: ${coffee.id}")
+            coffeeListener.onSelected(coffee.id)
+        }
     }
 
     companion object {
-        private const val TAG = "MainFragment"
         private const val ARG_PARAM1 = "param1"
         private const val ARG_PARAM2 = "param2"
+        private const val TAG = "ListFragment"
 
-        /**
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MainFragment.
-         */
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            MainFragment().apply {
+            ListFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
